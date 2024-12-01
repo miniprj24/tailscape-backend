b8215f02a301050b1bcf5ca87d8a2b7e992ddc01
Durvankur-Rajam <durvankurrajam099@gmail.com>
2024-12-02 03:57:37 +0530

    product CRUD implemented

--- controller/productController.js
+++ controller/productController.js
@@ -9,3 +9,37 @@ exports.getAllProducts = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+exports.createProduct = async (req, res) => {
+    try {
+      const product = new Product(req.body); // Assuming product data is passed in req.body
+      await product.save();
+      res.status(201).json({ message: 'Product created successfully', product });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
+  exports.updateProduct = async (req, res) => {
+    try {
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.status(200).json({ message: 'Product updated successfully', updatedProduct });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
+  exports.deleteProduct = async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
