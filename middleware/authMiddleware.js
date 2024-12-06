3a87d076fbb82605d7f797e42478fd534863bc37
nydevlearn2002@gmail.com <sp922113@gmail.com>
2024-12-06 15:33:24 +0530

    Product crud implemented

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,4 +13,11 @@ const authenticateUser = (req, res, next) => {
     }
 };
 
-module.exports = { authenticateUser };
+const checkAdminRole = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Access denied. Admins only' });
+    }
+    next();
+};
+
+module.exports = { authenticateUser, checkAdminRole };
