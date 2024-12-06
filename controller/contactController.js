ef68657d64018edc001be9ba1aec4c148c780bde
nydevlearn2002@gmail.com <sp922113@gmail.com>
2024-12-06 14:19:36 +0530

    Contact api updated

---
+++ controller/contactController.js
@@ -0,0 +1,25 @@
+const ContactMessage = require('../model/ContactMessage');
+
+exports.contactMessage = async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    if (!name || !email || !subject || !message) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    try {
+        const newMessage = new ContactMessage({
+            name,
+            email,
+            subject,
+            message,
+        });
+
+        await newMessage.save();
+
+        res.status(201).json({ message: 'Message saved successfully!' });
+    } catch (error) {
+        console.error('Error saving message:', error);
+        res.status(500).json({ error: 'Failed to save message. Please try again later.' });
+    }
+}
--- router/contactRouters.js
+++ router/contactRouters.js
@@ -1,34 +1,7 @@
-// router/contactRoutes.js
-
-
 const express = require('express');
 const router = express.Router();
-const ContactMessage = require('../model/ContactMessage'); 
+const { contactMessage } = require('../controller/contactController');
 
-router.post('/ContactMessage', async (req, res) => {
-  const { name, email, subject, message } = req.body;
-
-  if (!name || !email || !subject || !message) {
-    return res.status(400).json({ error: 'All fields are required.' });
-  }
-
-  try {
-    // Create a new contact message
-    const newMessage = new ContactMessage({
-      name,
-      email,
-      subject,
-      message,
-    });
-
-    // Save the message to the database
-    await newMessage.save();
-
-    res.status(201).json({ message: 'Message saved successfully!' });
-  } catch (error) {
-    console.error('Error saving message:', error);
-    res.status(500).json({ error: 'Failed to save message. Please try again later.' });
-  }
-});
+router.post('/contact-message', contactMessage);
 
 module.exports = router;
