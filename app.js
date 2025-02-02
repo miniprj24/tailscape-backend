2e7da6ee3696c11665f4d88fa8d45520f0d3f463
nydevlearn2002@gmail.com <sp922113@gmail.com>
2025-02-02 20:35:29 +0530

    updated CORS Policy with explicit headers

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/pets", petRoutes);
