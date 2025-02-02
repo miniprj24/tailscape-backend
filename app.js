d02ba2d37872023ef004fa48b76494de469e0463
nydevlearn2002@gmail.com <sp922113@gmail.com>
2025-02-02 20:17:17 +0530

    updated CORS policy

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const authRoutes = require('./router/authRoutes');
-const productRoutes = require('./router/productRoutes');
-const petRoutes = require('./router/petRoutes');
-const breedRoutes = require('./router/breedRoutes');
-const appointmentRoutes = require('./router/storeAppointmentRoutes');
-const contactRouters = require('./router/contactRouters');
-const vetHospitalRoutes = require('./router/vetHospitalRoutes');
-const doctorRoutes = require('./router/doctorRoutes');
-const doctorAppointmentRoutes=  require('./router/doctorAppointmentRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./router/authRoutes");
+const productRoutes = require("./router/productRoutes");
+const petRoutes = require("./router/petRoutes");
+const breedRoutes = require("./router/breedRoutes");
+const appointmentRoutes = require("./router/storeAppointmentRoutes");
+const contactRouters = require("./router/contactRouters");
+const vetHospitalRoutes = require("./router/vetHospitalRoutes");
+const doctorRoutes = require("./router/doctorRoutes");
+const doctorAppointmentRoutes = require("./router/doctorAppointmentRoutes");
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/pets', petRoutes);
-app.use('/api', breedRoutes);
-app.use('/api/appointments', appointmentRoutes);
-app.use('/api', contactRouters);
-app.use('/api/vetHospitals',vetHospitalRoutes);
-app.use('/api/doctor',doctorRoutes);
-app.use('/api/doctorAppointments', doctorAppointmentRoutes);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/pets", petRoutes);
+app.use("/api", breedRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api", contactRouters);
+app.use("/api/vetHospitals", vetHospitalRoutes);
+app.use("/api/doctor", doctorRoutes);
+app.use("/api/doctorAppointments", doctorAppointmentRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error("MongoDB connection error:", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = app;
