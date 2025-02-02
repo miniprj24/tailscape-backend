999f4fd9d0693a71c786e3c517042702d02a01b6
nydevlearn2002@gmail.com <sp922113@gmail.com>
2025-02-02 20:48:16 +0530

    Fixed API routes & CORS issues

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
